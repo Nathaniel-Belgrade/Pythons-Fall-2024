b3620e0157fa78f6287acd4bc9f1487b8c0fdbc5
Nathaniel-Belgrade <136392791+Nathaniel-Belgrade@users.noreply.github.com>
2024-12-09 23:34:51 -0500

    Update scrabble.py
    Added further explanation of what the code is meant to do.

--- scrabble.py
+++ scrabble.py
@@ -1,3 +1,9 @@
+#This program is a scrabble score calculator.
+#When ran, the program will ask for an input. You can put in any combination of letters.
+#After taking in your inputted "word", 
+#it will check each letter of the word, and find it's corresponding value within the dictionary
+#Finally, it will print the value to your inputted "word"
+
 #Alphabet storing all possible letters to a certain value
 alphabet = {
     'aeilnorstu':1,
@@ -6,7 +12,8 @@ alphabet = {
     'fhvwy':4,
     'k':5,
     'jx':8,
-    'qz':10
+    'qz':10,
+    '1234567890':0
 }
 #grabs inputted word, sets it to lowercase to match alphabet
 word = input("Input a word: ").lower()
